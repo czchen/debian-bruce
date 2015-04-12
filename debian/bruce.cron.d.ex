d7420a58129a3c28c8d95658f62926ace8f85a5a
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-04-12 21:24:48 +0800

    Remove unused template files

--- debian/bruce.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the bruce package
-#
-0 4	* * *	root	[ -x /usr/bin/bruce_maintenance ] && /usr/bin/bruce_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(bruce):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="bruce" command="/usr/bin/bruce"
